fc70577bfc2bf3035514ddd7d675caf01065515c
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-05 01:50:10 +0000

    Added HelloWorld.swift.

---
+++ HelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Joseph Wondimagnehu
+//  Created on 2025-09-04
+//  Version 1.0
+//  Copyright (c) 2025 Joseph Wondimagnehu. All rights reserved.
+//
+//  This program greets a friend and
+//  outputs an additional statement.
+
+// Greets Beni.
+print("\nHello, Beni!")
+// Outputs an additional statement.
+print("Java is the superior language! In my opinion, of course.\n")
